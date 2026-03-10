d727376a1b9f83373d3e1fb15181dde2bb3cfad2
A.Pranathi <pa0682@srmist.edu.in>
2026-03-10 08:11:49 +0530

    UC5: Array initialization with String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,59 +1,51 @@
-/**
- * OOPSBannerApp
- * Prints OOPS banner using String Array and Loop
- * @author AVULA PRANATHI
- * @version 4.0
- */
-public class OOPSBannerApp {
-
+class OOPSBannerApp {
     public static void main(String[] args) {
-
+        
         String[] banner = {
-
-            String.join("   ",
-                    " ***** ",
-                    " ***** ",
-                    " ***** ",
-                    " ***** "),
-
-            String.join("   ",
-                    "*     *",
-                    "*     *",
-                    "*     *",
-                    "*      "),
-
-            String.join("   ",
-                    "*     *",
-                    "*     *",
-                    "*     *",
-                    "*      "),
-
-            String.join("   ",
-                    "*     *",
-                    "*     *",
-                    " ***** ",
-                    " ***** "),
-
-            String.join("   ",
-                    "*     *",
-                    "*     *",
-                    "*      ",
-                    "      *"),
-
-            String.join("   ",
-                    "*     *",
-                    "*     *",
-                    "*      ",
-                    "      *"),
-
-            String.join("   ",
-                    " ***** ",
-                    " ***** ",
-                    "*      ",
-                    " ***** ")
+                String.join("   ",
+                        "  ***** ",
+                        "  ***** ",
+                        "  ***** ",
+                        "  ***** "
+                ),
+                String.join("   ",
+                        " *     *",
+                        " *     *",
+                        " *     *",
+                        " *      "
+                ),
+                String.join("   ",
+                        " *     *",
+                        " *     *",
+                        " *     *",
+                        " *      "
+                ),
+                String.join("   ",
+                        " *     *",
+                        " *     *",
+                        " ***** ",
+                        " ***** "
+                ),
+                String.join("   ",
+                        " *     *",
+                        " *     *",
+                        " *      ",
+                        "        *"
+                ),
+                String.join("   ",
+                        " *     *",
+                        " *     *",
+                        " *      ",
+                        "        *"
+                ),
+                String.join("   ",
+                        "  ***** ",
+                        "  ***** ",
+                        " *      ",
+                        " ***** "
+                )
         };
 
-        // Enhanced for-loop
         for (String line : banner) {
             System.out.println(line);
         }
